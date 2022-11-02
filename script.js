31bd84124486feafc9d23d4d38bf77d9f28cee54
Samir Jadhav <samirzjadhav@gmail.com>
2022-11-02 23:24:18 +0530

    feat: added media query on navbar

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const navbar = document.querySelector(".container-navbar");
+const bar = document.querySelector(".mobile");
+const close = document.querySelector(".close");
+const nav = document.querySelector(".navbar-menu");
 window.onscroll = () => {
   if (window.scrollY > 100) {
     navbar.classList.add("nav-active");
@@ -6,3 +9,14 @@ window.onscroll = () => {
     navbar.classList.remove("nav-active");
   }
 };
+
+if (bar) {
+  bar.addEventListener("click", () => {
+    nav.classList.add("active");
+  });
+}
+if (close) {
+  close.addEventListener("click", () => {
+    nav.classList.remove("active");
+  });
+}
